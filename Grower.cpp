3f1fe5d82219d3b2964119f35b89866d620c347a
Tiefen-boop <roi.tief97@gmail.com>
2021-05-14 18:39:56 +0300

    implemented Grower

---
+++ Grower.cpp
@@ -0,0 +1,19 @@
+//
+// Created by ikak on 14/05/2021.
+//
+
+#include <iostream>
+#include "Grower.h"
+
+Grower::Grower(std::string name, Gardener* gardener) : Person(name), gardener(gardener){}
+
+std::string Grower::getName() {
+    return "Grower " + Person::getName();
+}
+
+FlowerBouquet* Grower::prepareOrder(std::vector<std::string> flowers) {
+    std::cout << getName() << " forwards the request to " << gardener.getName() << ". " << std::endl;
+    FlowerBouquet* bouquet = gardener.prepareBouquet(flowers);
+    std::cout << gardener.getName() << " returns flowers to " << getName() << ". " << std::endl;
+    return bouquet;
+}
